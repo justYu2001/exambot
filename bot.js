bfba9bde99f7d61a52cd9417941930197a4a26ea
justYu2001 <49834964+justYu2001@users.noreply.github.com>
2019-08-23 07:26:02 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,6 +41,68 @@ function formula_img(src,msg)
         });    
 }
 
+//---------------------考試選擇-----------------------
+
+function select_exam(msg,e_arr,cmd_msg_id,last_msg_id)
+{
+    var del_flag=0;
+    msg.channel.awaitMessages(a_msg=>a_msg.content.toLocaleLowerCase().startsWith("s ")&&a_msg.author.id==msg.author.id,{maxMatches: 1,time:60*1000}).then(function(a_msg){
+        if(Number(a_msg.first().content.substr(2))>e_arr.length)
+        {
+            var embed=new Discord.RichEmbed();
+            var title_emojis=["608629862252412928","608618455905599488"];
+            str="沒有這個考試編號";
+            embed.addField(`${emoji(title_emojis[Math.floor(Math.random()*2)])}`+str,"請重新輸入",true);
+            embed.setColor(0xFF0000);
+            msg.channel.send({embed});
+            select_exa(msg,e_arr,cmd_msg_id,last_msg_id);
+            del_flag=1;
+        }
+        else
+        {
+            var embed=new Discord.RichEmbed();
+            e_arr=e_arr[Number(a_msg.first().content.substr(2))-1].split(",");
+            var date=e_arr[1].split(".");
+            var nd = new Date();
+            var gd = new Date();
+            nd.setHours(nd.getUTCHours()+8);
+            gd.setMonth(date[0]);
+            gd.setDate(date[1].split("(")[0]);
+            gd.setHours(0);
+            gd.setMinutes(0);
+            gd.setSeconds(0);
+            if(gd.getTime()-nd.getTime()>=24*60*60*1000)
+            {
+                var days= Math.floor((gd.getTime()-nd.getTime())/(24*60*60*1000));
+                embed.setColor(0x00FFCC);
+                embed.addField("**距離"+e_arr[0]+"還有"+days+"天**","範圍:\n"+e_arr[e_arr.length-1],false);
+                msg.channel.send({embed});
+            }
+            else if(gd.getTime()-nd.getTime()<24*60*60*1000&&gd.getTime()-nd.getTime()>0)
+            {
+                embed.setColor(0x00FFCC);
+                embed.addField("**距離"+e_arr[0]+"剩不到1天**","範圍:\n"+e_arr[e_arr.length-1],false);
+                msg.channel.send({embed});
+            }
+            else if(gd.getTime()-nd.getTime()<=0&&(nd.getTime()-gd.getTime())/(24*60*60*1000)<=e_arr.length-3)
+            {
+                var tt;
+                embed.setColor(0x00FFCC);
+                embed.addField("**"+e_arr[0]+"是今天**","範圍:\n"+tt,false);
+                msg.channel.send({embed});
+            }
+            del_flag=1;
+        }
+    });
+    client.setTimeout(function(){
+        if(!del_flag)
+        {
+            msg.channel.fetchMessage(cmd_msg_id).then(d_msg=>d_msg.delete());
+            msg.channel.fetchMessage(last_msg_id).then(d_msg=>d_msg.delete());
+        }
+    },60*1000);
+}
+
 //---------------------公式選擇----------------------
 
 function select_formula(msg,f_arr,cmd_msg_id,last_msg_id)
@@ -665,6 +727,7 @@ client.on('message', msg => {
      }
     if(msg.content.toLocaleLowerCase()=="ed")
     {
+        var cmd_msg_id=msg.id;
         var embed = new Discord.RichEmbed();
         fs.readFile('重要考試.txt', function (err, data) {
             if (err) throw err;
@@ -674,10 +737,12 @@ client.on('message', msg => {
             {
                 embed_content+=(i+1)+". "+str[i].split(",")[0]+"\n";
             }
-            embed_content+="\n\n請輸入s <你要查的考試編號>\n請在3分鐘內選擇你要查的考試";
+            embed_content+="\n\n請輸入s <你要查的考試編號>\n請在1分鐘內選擇你要查的考試";
             embed.addField("**即將到來的重要考試有.....**",embed_content,false);
             embed.setColor(0x0066FF);
-            msg.channel.send({embed});
+            msg.channel.send({embed}).then(last_msg=>{
+                select_exam(msg,str,cmd_msg_id,last_msg.id);
+            });
         });
     }
     if(msg.content.toLocaleLowerCase().startsWith("wf"))
@@ -792,45 +857,6 @@ client.on('message', msg => {
             });
         });
     }
-     if(msg.content.startsWith("<@!594719279673376780>")||msg.content.startsWith("<@594719279673376780>"))
-     {
-        let fullCommand = msg.content.substr(21)
-        let splitCommand = fullCommand.split(" ")
-        let primaryCommand = splitCommand[1] 
-        if(primaryCommand=="提醒我")
-        {/*
-            var by = splitCommand[2]>=today.getFullYear().toString();
-            var bmonth = splitCommand[3]>=today.getMonth().toString();
-            var bd = splitCommand[4]>=today.getDate().toString();*/
-            var nt = new Date();
-            var bh = Number(splitCommand[2])<=23;
-            var bmin = Number(splitCommand[3])<=59;
-            var h = (nt.getUTCHours()+8>24?nt.getUTCHours()+8-24:nt.getUTCHours()+8);
-            var now_time = h*60*60*1000+nt.getMinutes()*60*1000+nt.getSeconds()*1000;
-            console.log(h+":"+nt.getMinutes()+":"+nt.getSeconds());
-            var return_time =  Number(splitCommand[2])*60*60*1000+Number(splitCommand[3])*60*1000;
-            return_time-=now_time;
-            if(bh&&bmin&&(return_time>0))
-            {
-                client.setTimeout(function(){
-                    msg.channel.send(msg.author+"要"+splitCommand[splitCommand.length-1]+"了");
-                },return_time);
-                if(splitCommand[3].length<2)
-                {
-                    splitCommand[3] = "0"+splitCommand[3];
-                }
-                msg.channel.sendMessage("好!我會在"+splitCommand[2]+":"+splitCommand[3]+"提醒"+msg.author+"要"+splitCommand[splitCommand.length-1]);
-            }
-            else
-            {
-                msg.channel.sendMessage(msg.author+"時間格式錯誤，請重新輸入");
-            }
-        }
-        if(primaryCommand == "公式")
-        {
-            return_formula(splitCommand[2],msg);
-        }
-     }
     if(msg.content.toLowerCase() == "tt")
     {
         var t = new Date();
@@ -851,7 +877,8 @@ client.on('message', msg => {
                 }
                 else
                 {
-                    msg.channel.send(data.toString().substr(5));
+                    var str=data.toString().substr(5);
+                    msg.channel.send(str);
                 }
             });
         }
