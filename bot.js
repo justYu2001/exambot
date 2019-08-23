087ac9e5efe2617dd86c994601f73191824f5d14
justYu2001 <49834964+justYu2001@users.noreply.github.com>
2019-08-23 11:02:22 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,10 +4,10 @@ var request = require("request");
 const cheerio = require('cheerio');
 const client = new Discord.Client();
 const ping = require("./events/ping.js");
-const ed = require("./events/ed.js");
+const exam_date = require("./events/exam_date.js");
+const write_formula = require("./events/write_formula.js");
 const token = require("./token.js");
 client.login(token);
-//process.env.BOT_TOKEN'
 
 //-----------------表情符號------------------------
 
@@ -657,31 +657,8 @@ client.on('message', msg => {
         return;
     }
     ping(msg);
-    ed(msg,client);
-    if(msg.content.toLocaleLowerCase().startsWith("wf"))
-    {
-        var cmd=msg.content.substr(3);
-        var myserver = client.channels.get("612180981172142090");
-        const collector = new Discord.MessageCollector(myserver,m=>m.author.id=="134073775925886976",{maxMatches: 1,time:30*1000});
-        myserver.send("=tex "+cmd);
-        collector.on('collect',c_msg=>{
-            if(c_msg.content.includes("Rendering failed. Check your code. You may edit your existing message."))
-            {
-                const embed = new Discord.RichEmbed();
-                var title_emojis=["608629862252412928","608618455905599488"];
-                embed.addField(`${emoji(title_emojis[Math.floor(Math.random()*2)])}`+"**latex語法錯誤**","關於latex的語法請參考[這篇文章](https://walkccc.github.io/blog/2018/02/17/Techniques/latex-syntax/)",true);
-                embed.setColor(0xFF0000);
-                msg.channel.send({embed})
-            }
-            else
-            {
-                var Attachment = (c_msg.attachments).array();
-                Attachment.forEach(function(e){
-                    msg.channel.send({files:[e.url]});
-                });
-            }
-        });
-    }
+    exam_date(msg,client);
+    write_formula(msg,client);
     if(msg.content.toLocaleLowerCase().startsWith("calc"))
     {
         var cmd=msg.content.substr(5);
