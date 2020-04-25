8a5b82dd75b41760793b2bec87bd06e1c5375a16
justYu2001 <49834964+justYu2001@users.noreply.github.com>
2020-04-25 18:56:10 +0800

    Add files via upload

--- events/count_down.js
+++ events/count_down.js
@@ -1,25 +1,39 @@
-module.exports = (client) =>{
+module.exports = (client) => {
+  var gd = new Date();
+  gd.setFullYear(2020);
+  gd.setMonth(4);
+  gd.setDate(2);
+  gd.setHours(0);
+  gd.setMinutes(0);
+  gd.setSeconds(0);
 
-    var gd = new Date();
-    gd.setFullYear(2020);
-    gd.setMonth(4);
-    gd.setDate(2);
-    gd.setHours(0);
-    gd.setMinutes(0);
-    gd.setSeconds(0);
-
-    function game_activity()
-    {
-        var nd = new Date();
-        var time=gd.getTime()-(nd.getTime()+8*60*60*1000);
-        var d = parseInt(time/(24*60*60*1000));
-        var h = parseInt(time%(24*60*60*1000)/60/60/1000);
-        var m = parseInt((time%(24*60*60*1000)%(60*60*1000))/60/1000);/*
+  function game_activity() {
+    var nd = new Date();
+    var time = gd.getTime() - (nd.getTime() + 8 * 60 * 60 * 1000);
+    var d = parseInt(time / (24 * 60 * 60 * 1000));
+    var h = parseInt((time % (24 * 60 * 60 * 1000)) / 60 / 60 / 1000);
+    var m = parseInt(
+      ((time % (24 * 60 * 60 * 1000)) % (60 * 60 * 1000)) / 60 / 1000
+    ); /*
         var s = parseInt(time%(24*60*60*1000)%(60*60*1000)%(60*1000)/1000);*/
-        client.user.setActivity("統測倒數"+d+"天"+h+"時"+m+"分", {type: "PLAYING"});
+    if (time >= 0) {
+      client.user.setActivity('統測倒數' + d + '天' + h + '時' + m + '分', {
+        type: 'PLAYING',
+      });
+    } else {
+      client.user.setActivity('考統測囉!GO!GO!GO!', { type: 'PLAYING' });
     }
+    if (
+      (nd.getUTCHours() + 8) % 24 === 0 &&
+      nd.getUTCMinutes() === 0 &&
+      nd.getUTCSeconds() === 0
+    ) {
+      var c = client.channels.get('681171510609838100');
+      c.send(`<@&593404925753688064> 剩 ${d} 天`);
+    }
+  }
 
-    client.setInterval(function(){
-        game_activity();
-    },1000);
+  client.setInterval(function () {
+    game_activity();
+  }, 1000);
 };
